0960bee454be96caf5f97f0077276d4657374832
Cameron Bulock <cameron@bulock.com>
2012-04-25 17:39:31 -0400

    now pulls image selection from database

--- random.php
+++ random.php
@@ -1,10 +1,11 @@
 <?php
 require_once('settings.inc');
 
-$dir = ROOT_DIR.'/media/';
-$files = glob($dir.'*.*');
-$file = array_rand($files);
-$image = end(explode('/',$files[$file]));
+require_once(ROOT_DIR.'/common/db.php');
+$db = new DB('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 
-header('Location: '.WEB_ROOT.'display/'.$image);
+$sql = "SELECT filename FROM images WHERE display = '1' ORDER BY RAND() LIMIT 1";
+$result = $db->fetch($sql);
+
+header('Location: '.WEB_ROOT.'display/'.$result[0]['filename']);
 ?>
