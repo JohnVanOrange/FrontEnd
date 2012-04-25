1a53f6e6a320583d523a579edf91ce447b07e2f0
Cameron Bulock <cameron@bulock.com>
2012-04-25 16:42:35 -0400

    first step for database access.  Tool to import data in

--- common/common.php
+++ common/common.php
@@ -1,7 +1,12 @@
 <?php
-require_once('common/var.inc');
+define('SMARTY_DIR',ROOT_DIR.'/smarty/libs/');
+define('TEMPLATE_DIR',ROOT_DIR.'/templates');
+
 require_once(SMARTY_DIR.'Smarty.class.php');
 $tpl = new Smarty;
+
+require_once(ROOT_DIR.'/common/db.php');
+$db = new DB('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 
 $tpl->setTemplateDir(TEMPLATE_DIR);
 $tpl->setCacheDir(SMARTY_DIR.'../cache/');
---
+++ tools/insert_images_to_db.php
@@ -0,0 +1,30 @@
+<?php
+require_once('../settings.inc');
+require_once(ROOT_DIR.'/common/common.php');
+
+$dir = ROOT_DIR.'/media/';
+$files = glob($dir.'*.*');
+
+$sql = "INSERT INTO images(filename, hash, type, width, height) VALUES(:filename, :hash, :type, :width, :height)";
+
+foreach ($files as $file) {
+ echo "Importing ".$file."\n";
+ $filename = end(explode('/',$file));
+ $info = getimagesize($file);
+ $width = $info[0];
+ $height = $info[1];
+ $type = end(explode('/',$info['mime']));
+ $hash = md5_file($file);
+
+ $val = array(
+  ':filename' => $filename,
+  ':hash' => $hash,
+  ':type' => $type,
+  ':width' => $width,
+  ':height' => $height,
+ );
+ $s = $db->prepare($sql);
+ $s->execute($val);
+}
+
+?>
