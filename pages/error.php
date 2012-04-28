87d073494067decf53892402683a6a9d4786dddb
Cameron Bulock <cameron@bulock.com>
2012-04-28 16:47:23 -0400

    moved pages into pages directory

--- pages/error.php
+++ pages/error.php
@@ -1,6 +1,6 @@
 <?php
-require_once('settings.inc');
-require_once('common/smarty.php');
+require_once('../settings.inc');
+require_once('../common/smarty.php');
 
 $template = 'error.tpl';
 
--- pages/random.php
+++ pages/random.php
@@ -1,6 +1,6 @@
 <?php
-require_once('settings.inc');
-require_once('common/api.class.php');
+require_once('../settings.inc');
+require_once('../common/api.class.php');
 
 $api = new API;
 
