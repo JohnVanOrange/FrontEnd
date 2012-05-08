2da6822962dfeecd38eaa9b972b7b8d7fc74586d
Cameron Bulock <cameron@bulock.com>
2012-05-08 12:39:00 -0400

    new database tables for reports

---
+++ sql/reports.sql
@@ -0,0 +1,9 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+CREATE TABLE IF NOT EXISTS `reports` (
+  `id` int(5) unsigned NOT NULL auto_increment,
+  `image_id` int(10) unsigned NOT NULL,
+  `report_type` int(3) unsigned NOT NULL,
+  `resolved` tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
