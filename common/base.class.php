887a0af425e862467adc913a862b8448d6bfd76f
Cameron Bulock <cameron@bulock.com>
2012-06-14 16:22:21 -0400

    remove curl option for now

--- common/base.class.php
+++ common/base.class.php
@@ -16,7 +16,7 @@ class Base {
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 File Retrieval Bot by /u/cbulock (+'.WEB_ROOT.'bot)');
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+  //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE); //disable for now as prod server doesn't handle properly
   curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
   curl_setopt($ch, CURLOPT_TIMEOUT, 20);
   $response = curl_exec($ch);
