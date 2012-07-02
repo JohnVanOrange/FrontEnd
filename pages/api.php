f0e4e466d7d24bbbb699c276382ada143e3cb997
Cameron Bulock <cameron@bulock.com>
2012-07-02 16:17:09 -0400

    improved error handling

--- pages/api.php
+++ pages/api.php
@@ -1,33 +1,33 @@
 <?php
 
+header ('Content-type: application/json; charset=UTF-8');
+
 $valid_classes = array(
  'image' => 'Image',
- 'users' => 'Users',
+ 'user' => 'User',
  'tag' => 'Tag'
 );
 
-switch ($class) {
- case 'image':
- case 'users':
- case 'tag':
-  $class_name = $valid_classes[$class];
-  require_once('common/'.$class.'.class.php');
- break;
- default:
-  throw new Exception('Invalid class/URL');
- break;
-}
-
-$class_obj = new $class_name;
-
-header ('Content-type: application/json; charset=UTF-8');
-
 try {
+ switch ($class) {
+  case 'image':
+  case 'user':
+  case 'tag':
+   $class_name = $valid_classes[$class];
+   require_once('common/'.$class.'.class.php');
+  break;
+  default:
+   throw new Exception('Invalid class/URL');
+  break;
+ }
+ $class_obj = new $class_name;
  $result = $class_obj->{$method}($_REQUEST);
 }
 catch (exception $e) {
  js_exception_handler($e);
 }
+
+if (!is_array($result)) $result = array('response'=>$result);
 
 echo json_encode($result);
 ?>
