cddb48cb5ecd69301e5e24437ca961de61dcd4c3
Cameron Bulock <cameron@bulock.com>
2012-08-09 17:41:32 -0400

    fixed random.php after api change

--- pages/random.php
+++ pages/random.php
@@ -2,5 +2,7 @@
 $url = parse_url(WEB_ROOT);
 $tag = rtrim(str_replace($url['host'], '', $_SERVER['HTTP_HOST']),'.');
 
-header('Location: '.$url['scheme'].'://'.$_SERVER['HTTP_HOST'].'/v/'.call('image/random',array('tag'=>$tag)));
+$image = call('image/random',array('tag'=>$tag));
+
+header('Location: '.$url['scheme'].'://'.$_SERVER['HTTP_HOST'].'/v/'.$image['uid']);
 ?>
