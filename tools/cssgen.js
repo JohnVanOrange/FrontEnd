35fc429db0db560b7d71beb5253bb5d906dd4f65
Cameron Bulock <cameron@bulock.com>
2012-09-25 10:14:12 -0400

    tool to generate css from less

---
+++ tools/cssgen.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var theme_loc = '../css/themes/';
+
+var fs = require('fs');
+var sys = require('util');
+var exec = require('child_process').exec;
+
+var puts = function(err, stdout, stderr) {
+	if (stderr) {
+		throw stderr;
+	}
+	else {
+		sys.puts('CSS generated');
+	}
+}	
+
+dirlist = function(dir) {
+	var list = fs.readdirSync(dir);
+	var dirlist = [];
+	for(i in list) {
+		if (!~list[i].indexOf(".")) dirlist.push(list[i])
+	}
+	return dirlist;
+}
+
+dl = dirlist(theme_loc)
+
+for(i in dl) {
+	exec('lessc ' + theme_loc + dl[i] + '.less > ' + theme_loc + dl[i] + '/' + dl[i] + '.css', puts);
+}
+
