409c6cbd399204812ff94b50427520c9aacce2b0
Cameron Bulock <cameron@bulock.com>
2012-10-19 12:33:32 -0400

    trying to improve the math on the font size calc

--- pages/cloud.php
+++ pages/cloud.php
@@ -3,16 +3,30 @@ require('smarty.php');
 
 $template = 'cloud.tpl';
 
-$maxfont = '10';
+$maxfont = '60';
+$minfont = '10';
 
 $tags = call('tag/top');
 
 $max = current($tags);
 $min = end($tags);
 
+//echo 'max: '.$max['count'];
+//echo '<br>';
+//echo 'min: '.$min['count'];
+
 foreach ($tags as $i=>$tag)  {
- $tags[$i]['weight'] = ceil((($maxfont * ($tag['count'] - $min['count']) / $max['count'] - $min['count']) + 2) * 10);
+ //$tags[$i]['weight'] = ceil((($maxfont * ($tag['count'] - $min['count']) / $max['count'] - $min['count']) + 2) * 10);
+ if ($tag['count'] > $min['count']) {
+  $tags[$i]['weight'] = ceil($maxfont * ($tag['count'] - $min['count']) / $max['count'] - $min['count']) + $minfont;
+ }
+ else {
+  $tags[$i]['weight'] = $minfont;
+ }
+ //$tags[$i]['diff'] =  ($tag['count'] - $min['count']);
 }
+
+//print_r($tags);
 
 shuffle($tags);
 $tpl->assign('tags', $tags);
