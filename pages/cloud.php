59f0db72b5341e7f819865540ee8ea7c30107cb0
Cameron Bulock <cameron@bulock.com>
2012-10-19 12:44:46 -0400

    remove debugging

--- pages/cloud.php
+++ pages/cloud.php
@@ -11,23 +11,15 @@ $tags = call('tag/top');
 $max = current($tags);
 $min = end($tags);
 
-echo 'max: '.$max['count'];
-echo '<br>';
-echo 'min: '.$min['count'];
-
 foreach ($tags as $i=>$tag)  {
- //$tags[$i]['weight'] = ceil((($maxfont * ($tag['count'] - $min['count']) / $max['count'] - $min['count']) + 2) * 10);
  if ($tag['count'] > $min['count']) {
   $tags[$i]['diff'] =  ($tag['count'] - $min['count']);
-  $tags[$i]['weight'] = ceil(($maxfont * ($tag['count'] - $min['count'])) / ($max['count'] - $min['count'])) + $minfont;
+  $tags[$i]['weight'] = ceil((($maxfont - $minfont) * ($tag['count'] - $min['count'])) / ($max['count'] - $min['count'])) + $minfont;
  }
  else {
   $tags[$i]['weight'] = $minfont;
  }
- //$tags[$i]['diff'] =  ($tag['count'] - $min['count']);
 }
-
-print_r($tags);
 
 shuffle($tags);
 $tpl->assign('tags', $tags);
