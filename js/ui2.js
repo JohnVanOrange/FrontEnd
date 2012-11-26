2273a8ded1c36a6c4c543ea42a845613741f1530
Cameron Bulock <cameron@bulock.com>
2012-11-26 18:48:53 -0500

    make sure the js only catches keystrokes if ctrl isn't being held down.  Seems firefox would let the js capture these events when chrome wouldn't

--- js/ui2.js
+++ js/ui2.js
@@ -77,28 +77,38 @@ $(document).ready(function() {
  $.noty.defaults.timeout = 10000;
  
  /*Keyboard controls*/
+ var ctrl_down = false;
+
+ $(document).keydown(function(event) {
+  if (event.keyCode == 17) ctrl_down = true;
+ }).keyup(function(event) {
+  if (event.keyCode == 17) ctrl_down = false;
+ });
+ 
  $('body').keydown(function (event) {
-  switch (event.keyCode) {
-  case 32://Space
-   event.preventDefault();
-   $('.image').click();
-   break;
-  case 37://left arrow
-   window.history.back();
-   break;
-  case 39://right arrow
-   window.history.forward();
-   break;
-  case 83://s
-   $('#save_image').click();
-   break;
-  case 84://t
-   event.preventDefault();
-   $('#add_tag').click();
-   break;
-  case 124:
-   window.location.href = 'http://johnvanorange.com/v/joJpMJ';
-   break;
+  if (!ctrl_down) {
+   switch (event.keyCode) {
+   case 32://Space
+    event.preventDefault();
+    $('.image').click();
+    break;
+   case 37://left arrow
+    window.history.back();
+    break;
+   case 39://right arrow
+    window.history.forward();
+    break;
+   case 83://s
+    $('#save_image').click();
+    break;
+   case 84://t
+    event.preventDefault();
+    $('#add_tag').click();
+    break;
+   case 124:
+    window.location.href = 'http://johnvanorange.com/v/joJpMJ';
+    break;
+   }
   }
  });
  
