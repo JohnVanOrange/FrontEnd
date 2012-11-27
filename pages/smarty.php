8dc89aa095f229e8ca6acf448fcfd69e56302909
Cameron Bulock <cameron@bulock.com>
2012-11-27 15:59:32 -0500

    remove smarty from repo, have it installed via composer

--- pages/smarty.php
+++ pages/smarty.php
@@ -9,13 +9,9 @@ switch ($_COOKIE['mobile']) {
  break;
 }
 
-define('SMARTY_DIR',ROOT_DIR.'/smarty/libs/');
 define('TEMPLATE_DIR',ROOT_DIR.'/templates/'.$temptype);
 
-require_once(SMARTY_DIR.'Smarty.class.php');
 $tpl = new Smarty;
 
 $tpl->setTemplateDir(TEMPLATE_DIR);
-$tpl->setCacheDir(SMARTY_DIR.'../cache/');
-$tpl->setCompileDir(SMARTY_DIR.'../templates_c/');
 ?>
