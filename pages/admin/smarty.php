5e32e20e22dfdf1af81951673a4ffd1460e0838a
Cameron Bulock <cameron@bulock.com>
2012-11-28 11:58:37 -0500

    fixed admin pages to work with smarty change

--- pages/admin/smarty.php
+++ pages/admin/smarty.php
@@ -1,11 +1,7 @@
 <?php
-define('SMARTY_DIR',ROOT_DIR.'/smarty/libs/');
 define('TEMPLATE_DIR',ROOT_DIR.'/templates/admin');
 
-require_once(SMARTY_DIR.'Smarty.class.php');
 $tpl = new Smarty;
 
 $tpl->setTemplateDir(TEMPLATE_DIR);
-$tpl->setCacheDir(SMARTY_DIR.'../cache/');
-$tpl->setCompileDir(SMARTY_DIR.'../templates_c/');
 ?>
