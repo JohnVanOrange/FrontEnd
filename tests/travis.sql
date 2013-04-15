b97947eebb91bc0cea511afbfd50023ad6ed43cf
Cameron Bulock <cameron@bulock.com>
2013-04-15 14:41:00 -0400

    initial a user for testing

---
+++ tests/travis.sql
@@ -0,0 +1,18 @@
+INSERT INTO  `jvano_main`.`users` (
+`username` ,
+`password` ,
+`salt` ,
+`email` ,
+`type` ,
+`theme` ,
+`refresh`
+)
+VALUES (
+'testuser',
+'6df938a7ed2725c9327ebd44bd3ae281',
+'YnAZwgUGSP9WreOD',
+'test@example.com',
+'1',
+'light',
+'0'
+);
