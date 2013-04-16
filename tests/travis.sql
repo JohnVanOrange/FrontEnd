8b1df3b81cabde6e7238413999b4b08ba462b5c6
Cameron Bulock <cameron@bulock.com>
2013-04-16 18:19:32 -0400

    added admin user to travis sql

--- tests/travis.sql
+++ tests/travis.sql
@@ -1,3 +1,4 @@
+/* Standard user */
 INSERT INTO  `users` (
 `username` ,
 `password` ,
@@ -13,6 +14,25 @@ VALUES (
 'YnAZwgUGSP9WreOD',
 'test@example.com',
 '1',
+'light',
+'0'
+);
+/* Admin user */
+INSERT INTO  `users` (
+`username` ,
+`password` ,
+`salt` ,
+`email` ,
+`type` ,
+`theme` ,
+`refresh`
+)
+VALUES (
+'adminuser',
+'6df938a7ed2725c9327ebd44bd3ae281',
+'YnAZwgUGSP9WreOD',
+'admin@example.com',
+'2',
 'light',
 '0'
 );
