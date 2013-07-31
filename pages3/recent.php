bce6fbbd8878cdca395d1ecc4acd95e8c0e4f895
Cameron Bulock <cameron@bulock.com>
2013-07-31 18:02:44 -0400

    added pages for recent uploads and likes

---
+++ pages3/recent.php
@@ -0,0 +1,18 @@
+<?php
+namespace JohnVanOrange\jvo;
+
+require('twig.php');
+
+$api = new API();
+
+$data = [
+	'images'	=>	$api->call('image/recent'),
+	'title_text'	=>	'Recently Added Images'
+];
+
+require_once('common.php');
+
+header("Content-type: text/html; charset=UTF-8");
+
+$template = $twig->loadTemplate('thumbs.twig');
+echo $template->render($data);
