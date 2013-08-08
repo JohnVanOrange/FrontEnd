88625fa2cae54cd2495dfba0072f678914a736d5
Cameron Bulock <cameron@bulock.com>
2013-08-08 16:32:15 -0400

    updated hook to probably read the post data

--- tools/update-hook.php
+++ tools/update-hook.php
@@ -1,7 +1,9 @@
 <?php
 require_once(__DIR__.'/../settings.inc');
 
-if ($_POST['ref'] == '/refs/heads/'. BRANCH) {
+$payload = json_decode($_POST['payload']);
+
+if ($payload->ref == '/refs/heads/'. BRANCH) {
   echo '<pre>';
   $results = shell_exec('sudo -u ' . SERVER_USER . ' env PATH=$PATH ./fullupdate ' . BRANCH);
   echo $results;
