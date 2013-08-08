135c21da38d35f34c8803130d9dbe2730fb60a7d
Cameron Bulock <cameron@bulock.com>
2013-08-08 16:36:02 -0400

    another tweak to the hook

--- tools/update-hook.php
+++ tools/update-hook.php
@@ -3,7 +3,7 @@ require_once(__DIR__.'/../settings.inc');
 
 $payload = json_decode($_POST['payload']);
 
-if ($payload->ref == '/refs/heads/'. BRANCH) {
+if ($payload->ref == 'refs/heads/'. BRANCH) {
   echo '<pre>';
   $results = shell_exec('sudo -u ' . SERVER_USER . ' env PATH=$PATH ./fullupdate ' . BRANCH);
   echo $results;
@@ -19,6 +19,6 @@ else {
  mail(
   ADMIN_EMAIL,
   BRANCH . ' branch did not deploy on '. SITE_NAME,
-  print_r($_POST, 1)
+ $payload->ref
  );
 }
