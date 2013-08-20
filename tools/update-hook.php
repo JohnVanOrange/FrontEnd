00d7e65c4729130fcba3f005be6a04cb40b74498
Cameron Bulock <cameron@bulock.com>
2013-08-20 14:26:15 -0400

    improve path handling

--- tools/update-hook.php
+++ tools/update-hook.php
@@ -5,7 +5,7 @@ $payload = json_decode($_POST['payload']);
 
 if ($payload->ref == 'refs/heads/'. BRANCH) {
   echo '<pre>';
-  $results = shell_exec('sudo -u ' . SERVER_USER . ' env PATH=$PATH ../fullupdate ' . BRANCH);
+  $results = shell_exec('sudo -u ' . SERVER_USER . ' env PATH=$PATH cd ..; ./fullupdate ' . BRANCH);
   echo $results;
   mail(
    ADMIN_EMAIL,
