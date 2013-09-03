02cf36e65b969f4e3c59f0b73f08d0105f548419
Cameron Bulock <cameron@bulock.com>
2013-09-03 15:10:07 -0400

    have notification work with dropzone

--- js/add.js
+++ js/add.js
@@ -2,6 +2,9 @@ Dropzone.autoDiscover = false;
 $('document').ready(function(){
 	$("#drop").dropzone({
 		paramName: "image",
-		url: '/api/image/add'
+		url: '/api/image/add',
+		success: function(file, response) {
+			process(response);
+		}
 	});
 });
