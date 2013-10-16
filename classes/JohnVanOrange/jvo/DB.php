9635d431052b79e62dc592f99f674c23e61f839c
Cameron Bulock <cameron@bulock.com>
2013-10-15 20:39:33 -0400

    slight improvement in DB performance

--- classes/JohnVanOrange/jvo/DB.php
+++ classes/JohnVanOrange/jvo/DB.php
@@ -4,19 +4,29 @@ namespace JohnVanOrange\jvo;
 use PDO;
 
 class DB extends PDO {
+ 
+ private $dsn, $user, $pass, $dboptions, $setup;
 
  public function __construct($dsn, $user, $pass, $options=array()) {
-  parent::__construct($dsn, $user, $pass, $options);
-  $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-  $this->setAttribute(PDO::ATTR_PERSISTENT,TRUE);
+  $this->dsn = $dsn; $this->user = $user; $this->pass = $pass; $this->dboptions = $options; $this->setup = FALSE;
  }
 
  public function __destruct() {
  }
 
  public function fetch(\Peyote\Query $query) {
-  $s = $this->prepare($query->compile());
+  if (!$this->setup) $this->setup();
+  $q = $query->compile();
+  $s = $this->prepare($q);
   $s->execute($query->getParams());
   return $s->fetchAll();
  }
+
+ private function setup() {
+  $this->setup = TRUE;
+  parent::__construct($this->dsn, $this->user, $this->pass, $this->dboptions);
+  $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+  $this->setAttribute(PDO::ATTR_PERSISTENT,TRUE);
+ }
+ 
 }
