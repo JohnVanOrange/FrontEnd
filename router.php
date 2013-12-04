d7e1cc89e749ba8455f39248d115d6e498e31af0
Cameron Bulock <cameron@bulock.com>
2013-12-03 22:21:28 -0500

    added locale logic to router and setup i18n twig extension

--- router.php
+++ router.php
@@ -4,6 +4,13 @@ namespace JohnVanOrange\jvo;
 require_once('vendor/autoload.php');
 require_once('settings.inc');
 
+$locale = \Browser\Language::getLanguageLocale('_');
+
+putenv('LC_ALL=' . $locale);
+setlocale(LC_ALL, $locale);
+bindtextdomain('messages', ROOT_DIR.'/locale');
+bind_textdomain_codeset('messages', 'UTF-8');
+
 $map = json_decode(file_get_contents('router_map.json'));
 
 $route = new Route($_SERVER['REQUEST_URI'], $map);
