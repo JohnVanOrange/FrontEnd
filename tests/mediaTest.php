26f79599ba4b622973534eb7d50528fdc8e4fd78
Cameron Bulock <cameron@bulock.com>
2014-03-18 13:42:30 -0400

    updated media test to work with new hashing

--- tests/mediaTest.php
+++ tests/mediaTest.php
@@ -25,7 +25,7 @@ class mediaTest extends PHPUnit_Framework_TestCase {
   $result = $this->media->get($this->uid);
   $this->assertEquals($result['primary']['width'], '16', 'Width not correct');
   $this->assertEquals($result['primary']['height'], '16', 'Height not correct');
-  $this->assertEquals($result['primary']['hash'], '26e46ffc3d711d7b801d1eb78f3cc1ff', 'Hash not correct');
+  $this->assertEquals($result['primary']['hash'], 'a136aed2cd29fea21f53f46b9d3acbe3', 'Hash not correct');
   $this->assertEquals($result['primary']['size'], '3632', 'Size not correct');
   $this->assertEquals($result['primary']['format'], 'png', 'Format not correct');
   $this->assertEquals($result['primary']['file'], '/media/orange_slice16.png', 'File name not correct');
