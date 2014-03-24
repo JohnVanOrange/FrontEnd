fc09a791a317e7faa727e983a8d6ccab7d80e1ad
Cameron Bulock <cameron@bulock.com>
2014-03-23 20:18:44 -0400

    updated travis.sql

--- tests/travis/travis.sql
+++ tests/travis/travis.sql
@@ -4,18 +4,14 @@ INSERT INTO  `users` (
 `password` ,
 `salt` ,
 `email` ,
-`type` ,
-`theme` ,
-`refresh`
+`type`
 )
 VALUES (
 'testuser',
 '6df938a7ed2725c9327ebd44bd3ae281',
 'YnAZwgUGSP9WreOD',
 'test@example.com',
-'1',
-'light',
-'0'
+'1'
 );
 /* Admin user */
 INSERT INTO  `users` (
@@ -23,16 +19,12 @@ INSERT INTO  `users` (
 `password` ,
 `salt` ,
 `email` ,
-`type` ,
-`theme` ,
-`refresh`
+`type`
 )
 VALUES (
 'adminuser',
 '94feaf586c0d13d9359b7192ec1d6143',
 'F8udsbhmtcx4gMqW',
 'admin@example.com',
-'2',
-'light',
-'0'
+'2'
 );
