108111aa7b2bbcc085e646fcec74fa68a5271ffe
Cameron Bulock <cameron@bulock.com>
2014-11-25 21:21:15 -0500

    removed tag cloud from home page until it can be cached

--- src/jvo/Controller/Home.php
+++ src/jvo/Controller/Home.php
@@ -7,14 +7,14 @@ class Home extends Standard {
   
   $this->setTemplate('home');
   
-  $tc = new Components\TagCloud;
+  //$tc = new Components\TagCloud;
     
   $this->addData(
   [
    'rand'    =>    md5(uniqid(rand(), true)),
    'recent_likes'   =>  $this->api('image/recentLikes', ['count' => 8]),
-   'recent'   =>  $this->api('image/recent', ['count' => 8]),
-   'tags' => $tc->getData()
+   'recent'   =>  $this->api('image/recent', ['count' => 8])
+   //'tags' => $tc->getData()
   ]);
     
  }
