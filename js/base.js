fe6854ed78a6e20ceec08a9f801121fe596329f1
Cameron Bulock <cameron@bulock.com>
2015-02-05 16:48:36 -0500

    changed name of api debug function

--- js/base.js
+++ js/base.js
@@ -79,7 +79,7 @@ function process(result) {
  return null;
 }
 
-function debug(method, opt) {
+function jvo(method, opt) {
  var id = Math.floor((Math.random()*1000)+1);
  var start = +new Date();
  call(method,
