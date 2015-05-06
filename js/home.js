6af1cd134ddeb7f598330f8b0367553e5608110d
Cameron Bulock <cameron@bulock.com>
2015-05-06 18:17:24 -0400

    calcuate number of images to display on page based on window size

--- js/home.js
+++ js/home.js
@@ -1,16 +1,30 @@
 $(function () {
 	'use strict';
 
-	JVO.call('image/recentLikes', {count: 24}).done(function( response ) {
-		_.each(response, function( image ) {
-			$('#recent-likes').append( _.template($('#thumbnail').html())( {image: image} ) );
-		});
+	var $likes_containter = $('#recent-likes');
+	var likes_count = imageFit($likes_containter, 3);
+	JVO.call('image/recentLikes', {count: likes_count}).done(function( response ) {
+		addImages($likes_containter, response);
 	});
 
-	JVO.call('image/recent', {count: 24}).done(function( response ) {
-		_.each(response, function( image ) {
-			$('#recent-uploads').append( _.template($('#thumbnail').html())( {image: image} ) );
-		});
+	var $uploads_containter = $('#recent-uploads');
+	var uploads_count = imageFit($likes_containter, 3);
+	JVO.call('image/recent', {count: uploads_count}).done(function( response ) {
+		addImages($uploads_containter, response);
 	});
 
 });
+
+var addImages = function( containter, images) {
+	'use strict';
+	_.each(images, function( image ) {
+		containter.append( _.template($('#thumbnail').html())( {image: image} ) );
+	});
+};
+
+var imageFit = function( containter, rows ) {
+	'use strict';
+	var boxsize = containter.width();
+	var per_row = Math.floor(boxsize / 130);
+	return per_row * rows;
+};
