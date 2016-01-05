534f764a3fe98fc4d80a5307836ae78f1460d1e0
Cameron Bulock <cameron@bulock.com>
2016-01-05 10:46:07 -0500

    tweak to csp

--- src/jvo/SiteInterface/Standard.php
+++ src/jvo/SiteInterface/Standard.php
@@ -118,6 +118,7 @@ class Standard {
       "https://*.storage.googleapis.com",
       "https://*.objects.liquidweb.services",
       "https://www.gravatar.com",
+      "https://www.facebook.com",
       "https://stats.g.doubleclick.net",
       "https://assets.pinterest.com",
       "https://s-passets.pinimg.com"
